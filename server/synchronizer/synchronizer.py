89cc47a73d821d0076affad6e51b89a69fafc964
Adrian Maciej <amaciej06@gmail.com>
2020-05-25 00:18:56 +0200

    Pull, push, new synchronize(2) method

--- server/synchronizer/synchronizer.py
+++ server/synchronizer/synchronizer.py
@@ -5,35 +5,9 @@ from git import exc
 from git import Git
 
 
-repos = ['https://gitlab.com/RafalJuraszek/io-test']
-
-repo = Repo('C:\\Users\\Adrian\\Studia\\IoTest')
-print([r for r in repo.remotes])
-
-def synchronize():
-
-    # Repo object used to programmatically interact with Git repositories
-    #jesli plik ten jest w repo gita to bierze to repo (brak argumentu), mozna dac sciezke do folderu z gitem
-    repo = Repo('C:\\Users\\rafal\\Desktop\\semestr 6\\io\\test')
-    # check that the repository loaded correctly
-    if not repo.bare:
-
-        origin = repo.remotes['origin']
-        print(type(origin))
-        origin.pull()
-        for i, url_repo in enumerate(repos):
-            # najpeirw trzeba stworzyc (ale to bysmy pewnie brali z arajki
-            repo1 = repo.create_remote('v1repo{}'.format(i), url_repo)
-            #repo1 = repo.remote(name='v1repo{}'.format(i))
-
-            repo1.push()
-
-
-    else:
-        pass
-
 def log(message, lvl = 0):
     print(message)
+
 
 class SyncRepository:
     def __init__(self):
@@ -44,8 +18,16 @@ class SyncRepository:
     def initialize(self, localPath):
         try:
             self.localRepo = Repo(localPath)
-            self.origin = repo.remotes['origin']
-            self.remotes = [r for r in repo.remotes.items() if r is not self.origin]
+            if not self.localRepo.bare:                
+                for r in self.localRepo.remotes:
+                    print(type(r))
+                    print(r)
+                    if r.name != 'origin':
+                        self.remotes.append(r)
+                    else:
+                        self.origin = r                
+            else:
+                log(f'Repo not loaded correctly from {localPath}')
             return self
         except exc.InvalidGitRepositoryError:
             log(f'Invalid git repository in {localPath}')            
@@ -68,3 +50,45 @@ class SyncRepository:
 
     def generateRemoteName(self, remote:str):
         return remote
+
+    def pull(self):
+        self.origin.pull()
+
+    def pushToRemotes(self):
+        for remote in self.remotes:
+            remote.push('--force')
+
+
+repos = ['https://gitlab.com/RafalJuraszek/io-test']
+
+repo = Repo('C:\\Users\\Adrian\\Studia\\IoTest')
+print([r for r in repo.remotes])
+
+def synchronize():
+    # Repo object used to programmatically interact with Git repositories
+    #jesli plik ten jest w repo gita to bierze to repo (brak argumentu), mozna dac sciezke do folderu z gitem
+    repo = Repo('C:\\Users\\rafal\\Desktop\\semestr 6\\io\\test')
+    # check that the repository loaded correctly
+    if not repo.bare:
+
+        origin = repo.remotes['origin']
+        print(type(origin))
+        origin.pull()
+        for i, url_repo in enumerate(repos):
+            # najpeirw trzeba stworzyc (ale to bysmy pewnie brali z arajki
+            repo1 = repo.create_remote('v1repo{}'.format(i), url_repo)
+            #repo1 = repo.remote(name='v1repo{}'.format(i))
+
+            repo1.push()
+
+    else:
+        pass
+
+def synchronize2(syncRepo: SyncRepository):
+    syncRepo.pull()
+    syncRepo.pushToRemotes()
+
+repo = SyncRepository()
+repo.initialize('C:\\Users\\Adrian\\Studia\\IoTest')
+
+synchronize2(repo)
