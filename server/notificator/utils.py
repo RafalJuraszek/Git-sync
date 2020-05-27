042caa29736854674871eeb91efc2e2bd352f4fc
Lenovo <amaciej06@gmail.com>
2020-05-27 23:39:54 +0200

    Add endpoints handling

--- server/notificator/utils.py
+++
@@ -1,7 +0,0 @@
-from string import Template
-
-
-def read_template(filename):
-	with open(filename, 'r', encoding='utf-8') as template_file:
-		template_file_content = template_file.read()
-	return Template(template_file_content)
--- server/scrapper/scrapper_test_example.py
+++
@@ -1,5 +0,0 @@
-import scrapper
-repo_url = "https://github.com/mnabywan/Ontology"
-
-s = scrapper.Scrapper(repo_url)
-s.scrap_associated()
