9722e2ce657c113d4f29a801941b3b1457bb4bc8
kbieniasz <kbieniasz@onet.eu>
2020-06-09 11:59:42 +0200

    fix platform dependant path bug

--- server/db/database_localizator.py
+++ server/db/database_localizator.py
@@ -6,8 +6,11 @@ class DBLocation:
     def __init__(self):
         if expanduser("~") == "C:\\Users\\Lenovo":
             self.home_sql_lite = expanduser("~") + "\\kbieniasz_tmp\\projects\\Git-sync\\gitSync.db"
+        elif expanduser("~") == "C:\\Users\\kbien":
+            self.home_sql_lite = expanduser("~") + "\\PycharmProjects\\Git-sync\\gitSync.db"
         else:
-            self.home_sql_lite = expanduser("~") + "\\gitSync.db"
+            self.home_sql_lite = os.path.join(expanduser("~"), "gitSync.db")
+
 
 
 location = DBLocation()
